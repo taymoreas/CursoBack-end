2615fe5787b620f2b8fbcc760b76c8f7d02eea04
Tayna J. Moraes <125308160+taymoreas@users.noreply.github.com>
2023-03-13 14:07:14 -0300

    Delete Ex2.js

--- Logica de programação/Ex2.js
+++
@@ -1,9 +0,0 @@
-
-//Função "calcularsalario"
-
-function calcularsalario (horas, valorHoras){
-    salario = horas * valorHoras
-    console.log(`Seu salário é igual a: ${salario}`);
-}
-
-calcularsalario(150, 40.5)
