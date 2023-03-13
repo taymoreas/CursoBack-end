f228dbf0499221936423c06e93d51eb8698df528
Tayna J. Moraes <125308160+taymoreas@users.noreply.github.com>
2023-03-13 14:07:07 -0300

    Delete Ex.js

--- Logica de programação/Ex.js
+++
@@ -1,9 +0,0 @@
-
- //função "Olá!"
- 
- function calcularIdade (idadenumero){
-    idadeDias = idadenumero * 365
-       console.log(`Sua idade em dias é igual a: ${idadeDias}`);
-       
-   }
-    calcularIdade(25)
